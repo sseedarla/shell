5fc19a211e43e093a64c427a3c12a12fa52e3ba5
sseedarla <sseedarla.qa@gmail.com>
2024-02-27 04:16:42 +0530

    updated  mongodb file navigation details for modify file details

--- mongodb.sh
+++ mongodb.sh
@@ -6,9 +6,8 @@ dnf install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-cd /etc
 
-sed -i -e '10s|127.0.0.1|0.0.0.0|g' mongod.conf
+sed -i -e 's|127.0.0.1|0.0.0.0|g' /etc/mongod.conf
 
 systemctl restart mongod
 
