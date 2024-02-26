09b9583d3f1c88890ced2c231fc5aa14c249182a
sseedarla <sseedarla.qa@gmail.com>
2024-02-27 03:44:20 +0530

    new mongodb file conf and details added

---
+++ mongodb.sh
@@ -0,0 +1,12 @@
+cp mongodb.repo /etc/yum.repos.d/
+
+#Install MongoDB
+dnf install mongodb-org -y
+
+systemctl enable mongod
+systemctl start mongod
+
+sed -i -e '10s|127.0.0.1|0.0.0.0|g' mongod.conf
+
+systemctl restart mongod
+
