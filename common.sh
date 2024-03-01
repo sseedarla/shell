f58ed512ebe67fcc3326e683c48e70fe4df220e2
sseedarla <sseedarla.qa@gmail.com>
2024-03-02 04:57:16 +0530

    added new variables some common file to globlise the values

--- common.sh
+++ common.sh
@@ -1,14 +1,15 @@
 # this will print the file name
-hello=$0
-echo $hello
+#hello=$0
+#echo $hello
+
+# after entering the command then when you type anything but this will only take the 1st word
+# then you press enter then it will take as input inside the execution
+#miccheck=$1
+#echo ${miccheck}
 
 
-miccheck=$1
-echo ${miccheck}
-
-
-#hellobro=$*
-#echo $hellobro
+hellobro=$*
+echo $hellobro
 
 #avuna=$#
 #echo ${avuna}
