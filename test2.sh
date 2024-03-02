edfe340d1d7d4b1d1df987ee51e902092ef13559
sseedarla <sseedarla.qa@gmail.com>
2024-03-02 05:38:36 +0530

    added new variables some common file to globlise the values

---
+++ test2.sh
@@ -0,0 +1,20 @@
+# this will print the file name
+#hello=$0
+#echo $hello
+
+# when you have below command in you script it will ask for input, so in that case  after entering the command then when you type anything but this will only take the 1st word
+# (when you give same then it counts as word)
+# then you press enter then it will take as input inside the execution
+#miccheck=$1
+#echo ${miccheck}
+
+# when you have below command in you script it will ask for input, so in that case after  command and when you after any number of words your type as input then it takes
+#execute in input value
+#hellobro=$*
+#echo $hellobro
+
+# when you have any input taking command in your script then you might have to enter any input as the bash script command
+# then when you have this below command then it will show you how many number of argument you have passed into the script
+# every space after word is calculated   as one argument
+#avuna=$#
+#echo ${avuna}
