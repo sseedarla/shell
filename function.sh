d9c891bbf56032cce0bce5ba368ef4cc99a27aaf
sseedarla <sseedarla.qa@gmail.com>
2024-03-02 07:09:19 +0530

    playing with shell script functions

--- function.sh
+++ function.sh
@@ -13,14 +13,14 @@ ippudu_name
 ## We can send inputs to the function and we can access them with special variables $1-$n, $*, $#
 
 function_name1() {
-  a=234
+
   echo First Argument = $1
   echo Second Argument = $2
   echo All Arguments = $*
   echo No Of Arguments = $#
 }
 
-a=123
+
 function_name1 123 xyz
 
 # Functions will have its own exit status , but not with exit command, we have to use return command with exit states.
