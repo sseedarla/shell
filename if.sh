d75a7edc6cebf069d8522aed214d2e8077e4da29
sseedarla <sseedarla.qa@gmail.com>
2024-03-02 07:13:34 +0530

    playing with shell script functions

--- if.sh
+++ if.sh
@@ -1,4 +1,22 @@
-if [ 2+3=6 ]
+fruit_name=$1
+quantity=$2
+
+if [ -z "$fruit_name" ]; then
+  echo Input Missing
+  exit
+fi
+
+if [ "$fruit_name" == "mango" ]
 then
-  echo avunu 5
-  fi
+  echo Mango Quantity - $quantity
+else
+  echo Fruit does not exist
+fi
+
+## Always double quote variables in expressions
+
+if [ "$quantity" -gt 100 ]; then
+  echo Mango Price - 0.5$
+else
+  echo Mango Price - 1$
+fi
