2846f967e23ce7753e2d86eb64b31622a243e63f
sseedarla <sseedarla.qa@gmail.com>
2024-03-02 06:54:28 +0530

    playing with shell script variables

--- var.sh
+++ var.sh
@@ -1,15 +1,22 @@
+a=10
+name=DevOps
 
-# varable just to call the value
-name=sunil
-echo ${name}
+# Print Variable
+echo a = $a
+echo name = ${name}
 
+#
+#DATE=2023-04-18
+DATE=$(date +%F)
+echo Today date is ${DATE}
 
-# putting command inside and calling the command
-list=$(ls)
-echo $list
+ARTH=$((2-3*4/2))
 
-# adding some math
+echo ARTH = ${ARTH}
 
-samplemath=$((2+18))
-
-echo ${samplemath}
+# Special Variables for Inputs
+echo Script Name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo All Argument - $*
+echo No of Arguments - $#
