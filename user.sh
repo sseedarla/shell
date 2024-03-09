31e95422fc0a4867406628de1e121df45cdd4842
sseedarla <sseedarla.qa@gmail.com>
2024-03-09 16:52:13 +0530

    source file in common in all files

--- user.sh
+++ user.sh
@@ -1,10 +1,9 @@
 script_path=$(dirname $0)
-#source ${script_path}/common.sh
+source ${script_path}/common.sh
 echo ${script_path}
 #echo ${app_user}
 
-
-#echo -e "\e[37m»>>>>>>>> copy user.service to  /etc/systemd/system/<<<<<<<<\e[0m"
-
 #cp ${script_path}/user.service /etc/systemd/system/
+
+
 #Function1
