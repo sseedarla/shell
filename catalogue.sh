57fdde7dff16a46807529b930476750678fcf205
sseedarla <sseedarla.qa@gmail.com>
2024-03-10 18:22:06 +0530

    monogod code clean ry

--- catalogue.sh
+++ catalogue.sh
@@ -6,12 +6,12 @@ component=catalogue
 
 func_nodejs
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp ${script_path}/mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[37m»>>>>>>>> install nodejs <<<<<<<<\e[0m"
 
 dnf install mongodb-org-shell -y
 echo -e "\e[37m»>>>>>>>> import schema from mongo <<<<<<<<\e[0m"
 
-mongo --host mongodb-dev.sseedarla.tech</app/schema/catalogue.js
+mongo --host mongodb-dev.sseedarla.tech </app/schema/catalogue.js
 
 echo -e "\e[37m»>>>>>>>>  Huarry its done without any issues if you see this message !!<<<<<<<<\e[0m"
