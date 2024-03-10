aabb4ef5b6b842623d92e238eee28e4289ed3f9b
sseedarla <sseedarla.qa@gmail.com>
2024-03-10 18:18:23 +0530

    code dry to resualbel common file

--- catalogue.sh
+++ catalogue.sh
@@ -6,14 +6,12 @@ component=catalogue
 
 func_nodejs
 
-
-
-cp mongo.repo /etc/yum.repos.d/
+cp mongo.repo /etc/yum.repos.d/mongo.repo
 echo -e "\e[37m»>>>>>>>> install nodejs <<<<<<<<\e[0m"
 
 dnf install mongodb-org-shell -y
 echo -e "\e[37m»>>>>>>>> import schema from mongo <<<<<<<<\e[0m"
 
-mongo --host mongodb-dev.sseedarla.tech </app/schema/catalogue.js
+mongo --host mongodb-dev.sseedarla.tech</app/schema/catalogue.js
 
 echo -e "\e[37m»>>>>>>>>  Huarry its done without any issues if you see this message !!<<<<<<<<\e[0m"
