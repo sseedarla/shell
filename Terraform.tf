f2f7a88f6968411010b3ed79c6d437f41f4fa8af
sseedarla <sseedarla.qa@gmail.com>
2024-07-30 11:35:22 +0530

    terraform full instances

--- Terraform.tf
+++ Terraform.tf
@@ -1,7 +1,70 @@
-
-resource "aws_instance" "sample" {
-  ami                     = "ami-0b4f379183e5706b9"
-  instance_type           = "t3.nano"
-
-tags = { Name = "Sample"}
+data "aws_ami" "centos" {
+  most_recent = true
+  owners      = ["973714476881"]
+  name_regex  = "Centos-8-DevOps-Practice"
 }
+output "centos_ami" {
+  value = data.aws_ami.centos.image_id
+}
+
+resource "aws_instance" "frontend" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+tags = { Name = "frontend"}
+}
+
+
+resource "aws_instance" "mongodb" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "mongodb"}
+}
+
+resource "aws_instance" "catalogue" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "catalogue"}
+}
+
+resource "aws_instance" "shipping" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "shipping"}
+}
+
+resource "aws_instance" "user" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "user"}
+}
+
+resource "aws_instance" "cart" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "cart"}
+}
+
+resource "aws_instance" "payment" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "payment"}
+}
+
+resource "aws_instance" "redis" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "redis"}
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "rabbitmq"}
+}
+
+resource "aws_instance" "mysql" {
+  ami                     = data.aws_ami.centos.image_id
+  instance_type           = "t3.micro"
+  tags = { Name = "mysql"}
+}
+
