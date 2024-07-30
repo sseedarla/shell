e457bd76cc0f42ccf16a358de3652f9929f303e9
sseedarla <sseedarla.qa@gmail.com>
2024-07-30 12:06:57 +0530

    terraform full instances

--- Terraform.tf
+++ Terraform.tf
@@ -9,6 +9,13 @@ resource "aws_instance" "frontend" {
   instance_type           = "t3.micro"
 tags = { Name = "frontend"}
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "frontend-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
 
 
 resource "aws_instance" "mongodb" {
@@ -16,11 +23,25 @@ resource "aws_instance" "mongodb" {
   instance_type           = "t3.micro"
   tags = { Name = "mongodb"}
 }
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "mongodb-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
 
 resource "aws_instance" "catalogue" {
   ami                     = data.aws_ami.centos.image_id
   instance_type           = "t3.micro"
   tags = { Name = "catalogue"}
+}
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "catalogue-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
 }
 
 resource "aws_instance" "shipping" {
@@ -28,11 +49,25 @@ resource "aws_instance" "shipping" {
   instance_type           = "t3.micro"
   tags = { Name = "shipping"}
 }
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "shipping-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
 
 resource "aws_instance" "user" {
   ami                     = data.aws_ami.centos.image_id
   instance_type           = "t3.micro"
   tags = { Name = "user"}
+}
+resource "aws_route53_record" "user" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "user-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "cart" {
@@ -40,23 +75,54 @@ resource "aws_instance" "cart" {
   instance_type           = "t3.micro"
   tags = { Name = "cart"}
 }
+resource "aws_route53_record" "cart" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "cart-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
 
 resource "aws_instance" "payment" {
   ami                     = data.aws_ami.centos.image_id
   instance_type           = "t3.micro"
   tags = { Name = "payment"}
 }
+resource "aws_route53_record" "payment" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "payment-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
 
 resource "aws_instance" "redis" {
   ami                     = data.aws_ami.centos.image_id
   instance_type           = "t3.micro"
   tags = { Name = "redis"}
 }
+resource "aws_route53_record" "redis" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "redis-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
 
 resource "aws_instance" "rabbitmq" {
   ami                     = data.aws_ami.centos.image_id
   instance_type           = "t3.micro"
   tags = { Name = "rabbitmq"}
+}
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "rabbitmq-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -64,4 +130,10 @@ resource "aws_instance" "mysql" {
   instance_type           = "t3.micro"
   tags = { Name = "mysql"}
 }
-
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z003289722MQNM42L3N3R"
+  name    = "mysql-dev.sseedarla.tech"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
