91b95560f15a3825a31094020883d5823769f740
sunil <sseedarla.qa@gmail.com>
2025-02-12 17:52:34 +0530

    updated latest changes

--- payment.sh
+++ payment.sh
@@ -1,10 +1,8 @@
 script=$(realpath "$0")
 script_path=$(dirname "$script")
 source ${script_path}/common.sh
-rabbitmq_appuser_password=$1
+rabbit_appuser_password=$1
 #roboshop123
-#while entering you have to enter " sudo bash payment.sh roboshop123 " in the command line while you run this payment.sh bash file
-
 component=payment
 
 if [ -z "$rabbitmq_appuser_password" ]; then
@@ -28,7 +26,7 @@ cd /app
 unzip /tmp/${component}.zip
 echo -e "\e[37m»>>>>>>>>  copy  payment.service to its location /etc/systemd/system/  <<<<<<<<\e[0m"
 
-sed -i -e "s|rabbit_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/${component}.service
+sed -i -e "s|rabbit_appuser_password|${rabbitmq_appuser_password}|" ${script_path}/payment.service
 cp ${script_path}/${component}.service /etc/systemd/system/${component}.service
 echo -e "\e[37m»>>>>>>>> install -r requirements.txt   <<<<<<<<\e[0m"
 
