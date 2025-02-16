5923ce0148428c72d0e77332a3ce5846636ae8b5
sunil <sseedarla.qa@gmail.com>
2025-02-16 07:43:47 +0530

    run file to install all at once

---
+++ runall.sh
@@ -0,0 +1,14 @@
+sudo bash Cart.sh
+sudo bash Catalogue.sh
+sudo bash Dispatch.sh
+sudo bash Frontend.sh
+sudo bash MongoDB.sh
+sudo bash MySQL.sh
+sudo bash Payment.sh
+sudo bash RabbitMQ.sh
+sudo bash Redis.sh
+sudo bash Shipping.sh
+sudo bash User.sh
+
+
+
