e13c3a9a44342434376d281aad0f5108407328fc
sunil <sseedarla.qa@gmail.com>
2025-02-17 08:01:00 +0530

    monogo db shell updates

--- runall.sh
+++ runall.sh
@@ -4,25 +4,25 @@ sudo rm -rf mongo.repo
 sudo mv mongo.repo /etc/yum.repos.d/
 sudo bash mongodb.sh
 
-#sudo mv mysql.repo /etc/yum.repos.d/
-#sudo bash MySQL.sh
+sudo mv mysql.repo /etc/yum.repos.d/
+sudo bash MySQL.sh
 
 
-#sudo bash Cart.sh
+sudo bash Cart.sh
 
-#sudo mv catalogue.service /etc/systemd/system/
-#sudo bash Catalogue.sh
-#sudo bash Dispatch.sh
+sudo mv catalogue.service /etc/systemd/system/
+sudo bash Catalogue.sh
+sudo bash Dispatch.sh
 
-#sudo mv roboshop.conf /etc/nginx/default.d/
-#sudo bash Frontend.sh
+sudo mv roboshop.conf /etc/nginx/default.d/
+sudo bash Frontend.sh
 
 
-#sudo bash Payment.sh
-#sudo bash RabbitMQ.sh
-#sudo bash Redis.sh
-##sudo bash Shipping.sh
-#sudo bash User.sh
+sudo bash Payment.sh
+sudo bash RabbitMQ.sh
+sudo bash Redis.sh
+#sudo bash Shipping.sh
+sudo bash User.sh
 
 
 
