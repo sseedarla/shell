a1635951832ed52cbb7c5447ad048532c11323a8
sunil <sseedarla.qa@gmail.com>
2025-03-10 06:44:21 +0530

    terraform initillation

--- aws.tf
+++ aws.tf
@@ -1,4 +1,8 @@
 resource "aws_instance" "webapp" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
 }
+
--- it.sh
+++ it.sh
@@ -3,3 +3,6 @@ sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
 
 sudo yum -y install terraform
+
+terraform init
+
