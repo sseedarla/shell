8698835f88622b2f6756a9492b8c3845aa3616a5
sunil <sseedarla.qa@gmail.com>
2025-03-12 01:26:37 +0530

    terraform servers

--- servers.tf
+++ servers.tf
@@ -7,12 +7,26 @@ terraform {
   }
 }
 
+
 data "aws_ami" "centos" {
-  owners = 973714476881
+  owners = ["973714476881"]
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
 
 }
+
+
+resource "aws_instance" "webapp" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
+
 
 variable "instance_type" {
   default = "t2.micro"
