ac5248c1b0af4089fa143f9f0e3c372eb78d382c
sunil <sseedarla.qa@gmail.com>
2025-03-13 10:20:11 +0530

    terraform servers

--- servers.tf
+++ servers.tf
@@ -16,17 +16,103 @@ data "aws_ami" "centos" {
 }
 
 
-variable "instance_type" {
+/*variable "" {
   default = "t2.micro"
-}
+}*/
 
 data "aws_security_group" "allow-all" {
   name = "allow-all"
 
 }
 
+variable "components" {
+  default = {
+
+    frontend =
+    {
+  name          = "frontend"
+  instance_type = "t2.micro"
+}
+
+mongodb=
+{
+  name          = "mongodb"
+  instance_type = "t2.micro"
+}
+
+catalogue=
+{
+  name          = "catalogue"
+  instance_type = "t2.micro"
+}
+
+redis=
+{
+  name          = "redis"
+  instance_type = "t2.micro"
+}
+user=
+{
+  name          = "user"
+  instance_type = "t2.micro"
+}
+cart=
+{
+  name          = "cart"
+  instance_type = "t2.micro"
+}
+mysql=
+{
+  name          = "mysql"
+  instance_type = "t2.micro"
+}
+shipping=
+{
+  name          = "shipping"
+  instance_type = "t2.micro"
+}
+rabbitmq=
+{
+  name          = "rabbitmq"
+  instance_type = "t2.micro"
+}
+payment=
+{
+  name          = "payment"
+  instance_type = "t2.micro"
+}
+dispatch=
+{
+  name          = "dispatch"
+  instance_type = "t2.micro"
+}
+
+}
+}
+
+resource "aws_instance" "instance" {
+
+  for_each = var.components
+  ami           = data.aws_ami.centos.image_id
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+resource "aws_route53_record" "records" {
+  for_each = var.components
+
+  zone_id = "Z05653472ND08OLE086CI"
+  name    = "${each.value["name"]}-dev.sseedarla.site"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance[each.value["name"]].private_ip]
+}
 
 
+/*
 resource "aws_instance" "Frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = var.instance_type
@@ -216,5 +302,5 @@ resource "aws_route53_record" "Dispatch" {
   ttl     = 30
   records = [aws_instance.Dispatch.private_ip]
 }
-
+*/
 
