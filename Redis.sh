563326b7e99a1bac7446ea30c651c6d57673fb48
sunil <sseedarla.qa@gmail.com>
2025-08-05 04:42:32 +0530

    user.tf

--- Redis.sh
+++ Redis.sh
@@ -4,6 +4,10 @@ dnf module enable redis:remi-6.2 -y
 
 dnf install redis -y
 
+sudo sed -i 's/bind 127.0.0.1/bind 0.0.0.0/' /etc/redis.conf
+
+
+
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/redis.conf & /etc/redis/redis.conf
 # You can edit file by using vim /etc/redis.conf & vim /etc/redis/redis.conf
 
