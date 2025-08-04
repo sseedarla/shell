469291be3b6daa65eec4e99c0b5b36bb7643f991
sunil <sseedarla.qa@gmail.com>
2025-08-04 18:04:35 +0530

    front changes

--- Frontend.sh
+++ Frontend.sh
@@ -10,7 +10,10 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-vim /etc/nginx/default.d/roboshop.conf
+
+sudo mv roboshop.conf /etc/nginx/default.d/
+
+#vim /etc/nginx/default.d/roboshop.conf
 
 
 
