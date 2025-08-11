8cb9d035857a53913cb5fe61695dd11d30626517
sunil <sseedarla.qa@gmail.com>
2025-08-12 03:07:05 +0530

    File changes

--- servers.tf
+++ servers.tf
@@ -28,77 +28,70 @@ data "aws_security_group" "allow-all" {
 variable "components" {
   default = {
 
-    frontend= {
-  name          = "frontend"
-  instance_type = "t2.micro"
-}
+    frontend = {
+      name          = "frontend"
+      instance_type = "t2.micro"
+    }
 
-mongodb= {
-  name          = "mongodb"
-  instance_type = "t2.micro"
-}
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t2.micro"
+    }
 
-catalogue= {
-  name          = "catalogue"
-  instance_type = "t2.micro"
-}
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t2.micro"
+    }
 
-    # here is the ending on the compoents loop below
+
+
+    redis = {
+      name          = "redis"
+      instance_type = "t2.micro"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t2.micro"
+    }
+
+    cart = {
+      name          = "cart"
+      instance_type = "t2.micro"
+    }
+
+    mysql = {
+      name          = "mysql"
+      instance_type = "t2.micro"
+    }
+
+    shipping = {
+      name          = "shipping"
+      instance_type = "t2.micro"
+    }
+
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t2.micro"
+    }
+
+
+    payment = {
+      name          = "payment"
+      instance_type = "t2.micro"
+    }
+
+    dispatch = {
+      name          = "dispatch"
+      instance_type = "t2.micro"
+    }
+
   }
 }
-
-/*
-
-redis= {
-  name          = "redis"
-  instance_type = "t2.micro"
-}
-user= {
-  name          = "user"
-  instance_type = "t2.micro"
-}
-
-cart= {
-  name          = "cart"
-  instance_type = "t2.micro"
-}
-
-mysql= {
-  name          = "mysql"
-  instance_type = "t2.micro"
-}
-
-shipping= {
-  name          = "shipping"
-  instance_type = "t2.micro"
-}
-
-rabbitmq= {
-  name          = "rabbitmq"
-  instance_type = "t2.micro"
-}
-
-
-payment= {
-  name          = "payment"
-  instance_type = "t2.micro"
-}
-
-dispatch= {
-  name          = "dispatch"
-  instance_type = "t2.micro"
-}
-
-
-
-
- */
-
 
 
 resource "aws_instance" "instance" {
 
-  for_each = var.components
+  for_each      = var.components
   ami           = data.aws_ami.centos.image_id
   instance_type = each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
